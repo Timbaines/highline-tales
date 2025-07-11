77916a353ca4a4244b8083b1f6da68b23c6c555a
Tim Baines <timbaines@timbaines.com>
2025-07-10 21:05:27 -0400

    Organize styles: move `globals.css` and add `typography.css`

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import './globals.css'
+import './styles/globals.css'
+import './styles/typography.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

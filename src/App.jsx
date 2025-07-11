a58d84c804cf527b0cdf4e54c822139093772cab
Tim Baines <timbaines@timbaines.com>
2025-07-10 21:05:01 -0400

    Set up project structure with dependencies, scripts, and initial components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import Header from './components/Header';
+
+import './styles/globals.css'
 
 export default function App() {
 
   return (
     <>
-
+      <Header />
+        <section>
+            <div className="gridContainer">
+                <div className="trailContainer">
+                    <h2>Highline Trail</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum esse ex explicabo inventore minima! At cumque delectus, dignissimos dolore ea est excepturi fuga in numquam provident quis saepe, vel!</p>
+                </div>
+                <div className="weatherContainer">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum esse ex explicabo inventore minima! At cumque delectus, dignissimos dolore ea est excepturi fuga in numquam provident quis saepe, vel!</p>
+                </div>
+            </div>
+        </section>
     </>
   )
 };

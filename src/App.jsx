cc52d2d83f6deb39151a0898c9af80f5f71a4ef4
Tim Baines <timbaines@timbaines.com>
2025-07-12 05:29:51 -0400

    refactor: rename `Card` prop from `activity` to `adventure` for clarity and consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Header from './components/Header';
 import Card from './components/Card';
-import { activities } from './data/activities';
+import WeatherCard from './components/ui/WeatherCard';
+
+import { adventureData } from './data/adventure-data.js';
+
 import './styles/globals.css';
 
 export default function App() {
@@ -9,27 +12,23 @@ export default function App() {
     <>
       <Header />
         <main>
-            <section>
-                <div className="gridContainer">
-                    <div className="trailContainer">
-                        <h2>Our Destination</h2>
-                        <div className="trailInfo">
-                            <p>Glacier National Park in Montana is an immense park spanning over 1 million acres. It is considered the centerpiece of a vast region of protected land between the United States and Canada. The park features over 700 miles of hiking trails and supports various outdoor activities such as biking, boating, fishing, and horseback riding. It is known for its dazzling hikes, backcountry campsites, glaciers, peaks, and lakes. The park is best visited during the summer months between June and September.</p>
-                        </div>
-                        <h3>Activities</h3>
-
-                        {activities.map(activity => (
-                            <Card key={activity.id} activity={activity} />
-                        ))}
-
+            <div className="gridContainer">
+                <div className="trailContainer">
+                    <h2>Our Destination</h2>
+                    <div className="trailInfo">
+                        <p>This years upcoming vacation destination is Montana, where my wife and I will be staying in Whitefish and exploring the breathtaking Glacier National Park. Spanning over more than 1 million acres, Glacier is the crown jewel of a vast protected wilderness shared by the U.S. and Canada. With more than 700 miles of hiking trails, the park offers endless opportunities for adventure—from scenic hikes and boating on alpine lakes to wildlife viewing and relaxing drives. We’re excited to experience its iconic glaciers, rugged peaks, and crystal-clear lakes during the summer months, which are the ideal time to visit.</p>
                     </div>
-                    <div className="weatherContainer">
-                        <div className="weatherInfo">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum esse ex explicabo inventore minima! At cumque delectus, dignissimos dolore ea est excepturi fuga in numquam provident quis saepe, vel!</p>
-                        </div>
-                    </div>
+                    <h3>Adventures</h3>
+
+                    {adventureData.map(adventure => (
+                        <Card key={adventure.id} adventure={adventure} />
+                    ))}
+
                 </div>
-            </section>
+                <div className="weatherContainer">
+                    <WeatherCard />
+                </div>
+            </div>
         </main>
     </>
   )

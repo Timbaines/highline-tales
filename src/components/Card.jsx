5c9227fd406550ad2d619e5994a9437c06d083ba
Tim Baines <timbaines@timbaines.com>
2025-07-12 05:29:48 -0400

    refactor: rename `Card` prop from `activity` to `adventure` for clarity and consistency

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,25 +2,25 @@ import { FaRegHeart } from 'react-icons/fa';
 
 import styles from './Card.module.css';
 
-export default function Card({ activity }) {
+export default function Card({ adventure }) {
     return (
         <article className={styles.card}>
             <div className={styles.cardImageContainer}>
                 <img
                     className={styles.cardImage}
-                    src={activity.image}
-                    alt={activity.title}
+                    src={adventure.image}
+                    alt={adventure.title}
                 />
             </div>
             <div className={styles.cardContent}>
                 <h3 className={styles.cardTitle}>
-                    <a>{activity.title}</a>
+                    <a>{adventure.title}</a>
                 </h3>
                 <p className={styles.cardStats}>
-                    Date: {activity.date} | Difficulty: {activity.difficulty} | Miles: {activity.miles}
+                    Date: {adventure.date} | Difficulty: {adventure.difficulty} | Miles: {adventure.miles}
                 </p>
                 <p className={styles.cardDescription}>
-                    {activity.description}
+                    {adventure.description}
                 </p>
                 <div className={styles.cardActions}>
                     <a className={styles.cardLink} href="/">Read More →</a>

4ef668bc574b8acbb2aa1900d10523cea9ae6f9d
Tim Baines <timbaines@timbaines.com>
2025-07-17 05:42:13 -0400

    style: refine `Card` component styles for hover effects and spacing
    - Add hover state styling for `.card` and adjust spacing for `.cardStats`
    - Update `.cardLikes` gap, font size, and add `user-select: none`
    - Refine `.likeCounter` font size, color, and spacing adjustments
    - Improve mobile styles with added margins and hover adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Header from '@/components/Header';
-import Card from '@/components/ui/Card.jsx';
-import WeatherCard from '@/components/ui/WeatherCard';
+import Destination from '@/components/Destination'
+import Activities from '@/components/Activities';
+import Footer from '@/components/Footer';
 
-import { adventureData } from '@/data/adventure-data.js';
-
+/***** GLOBAL STYLES *****/
 import './styles/globals.css';
 
 export default function App() {
@@ -12,26 +12,16 @@ export default function App() {
     <>
       <Header />
         <main>
-            <div className="gridContainer">
-                <div className="trailContainer">
-                    <h2>Our Destination</h2>
-                    <div className="trailInfo">
-                        <p>This years upcoming vacation destination is Montana, where my wife and I will be staying in Whitefish and exploring the breathtaking Glacier National Park. Spanning over more than 1 million acres, Glacier is the crown jewel of a vast protected wilderness shared by the U.S. and Canada. With more than 700 miles of hiking trails, the park offers endless opportunities for adventure—from scenic hikes and boating on alpine lakes to wildlife viewing and relaxing drives. We’re excited to experience its iconic glaciers, rugged peaks, and crystal-clear lakes during the summer months, which are the ideal time to visit.</p>
-                    </div>
-                    <h3>Adventures</h3>
-
-                    {adventureData.map(adventure => (
-                        <Card key={adventure.id} adventure={adventure} />
-                    ))}
-
-                </div>
-                <div className="weatherContainer">
-                    <WeatherCard />
-                </div>
-            </div>
+            <section>
+                {/*DESTINATION COMPONENT*/}
+               <Destination />
+            </section>
+            <section>
+                {/*ACTIVITIES COMPONENT*/}
+                <Activities />
+            </section>
         </main>
+        <Footer />
     </>
   )
 };
-
-

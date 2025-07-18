4a36dc90d8bdc4f43ddb6c79911b983466ed4277
Tim Baines <timbaines@timbaines.com>
2025-07-18 02:28:59 -0400

    feat: add `ThemeToggle` component with dark/light mode switch
    - Create `ThemeToggle.jsx` for toggling between themes using `useTheme` hook
    - Add `ThemeToggle.module.css` for button styling with accessible design
    - Use React icons (`FiSun` and `FiMoon`) to represent themes visually

---
+++ src/components/ui/ThemeToggle.jsx
@@ -0,0 +1,22 @@
+import { useTheme } from "@/context/ThemeContext.jsx"
+import { FiSun, FiMoon } from "react-icons/fi";
+
+/***** MODULE STYLES *****/
+import styles from "./ThemeToggle.module.css";
+
+const ThemeToggle = ({ className, ...props }) => {
+    const { isDarkMode, toggleTheme } = useTheme();
+
+    return (
+        <button
+            type="button"
+            onClick={toggleTheme}
+            className={`${styles.themetoggle} ${className || ''}`}
+            {...props}
+        >
+            {isDarkMode ? <FiSun size={14} /> : <FiMoon size={14} />}
+        </button>
+    )
+}
+
+export default ThemeToggle;

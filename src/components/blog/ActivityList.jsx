00e975c842aa0825ccfd1ea203a0bb52492008db
Tim Baines <timbaines@timbaines.com>
2025-07-18 08:12:43 -0400

    refactor: move `ActivityList` to `blog` directory and update imports
    - Rename and relocate `ActivityList` and its styling file to the `blog` directory for better structure.
    - Update imports in `ActivityList.jsx` for consistency.
    - Replace `adventure` with `activity` in variable names for naming coherence.

--- src/components/blog/ActivityList.jsx
+++ src/components/blog/ActivityList.jsx
@@ -1,16 +1,16 @@
 import ActivityCard from '@/components/blog/ActivityCard.jsx';
-import {activitiesData} from '@/data/activities-data.js';
+import { activitiesData } from '@/data/activities-data.js';
 
 /***** MODULE STYLES *****/
-import styles from '@/components/ActivityList.module.css'
+import styles from '@/components/blog/ActivityList.module.css'
 
 export default function ActivityList() {
     return (
         <div className={styles.activityListGridContainer}>
             <div>
-                <h3>Adventures</h3>
-                {activitiesData.map(adventure => (
-                    <ActivityCard key={adventure.id} adventure={adventure} />
+                <h3>Activities</h3>
+                {activitiesData.map(activity => (
+                    <ActivityCard key={activity.id} activity={activity} />
                 ))}
             </div>
         </div>

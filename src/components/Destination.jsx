556e2c3c8b451319de05501fbcd22066fd394a4d
Tim Baines <timbaines@timbaines.com>
2025-07-18 08:12:20 -0400

    style: restructure `Destination` content and update styles for spacing consistency
    - Wrap destination text sections in separate `<div>` blocks for better layout management.
    - Add `.destinationRow` CSS class to enhance spacing between sections.

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -8,9 +8,12 @@ export default function Destination() {
         <div className={styles.destinationGridContainer}>
             <div>
                 <h2>Our Destination</h2>
-                <p>This years upcoming vacation destination is Montana, where Sarah and I will be staying in Whitefish and exploring the Glacier National Park. Spanning over more than 1 million acres, Glacier is the crown jewel of a vast protected wilderness shared by the U.S. and Canada.</p>
-
-                <p>With more than 700 miles of hiking trails, the park offers endless opportunities for adventure—from scenic hikes and boating on alpine lakes to wildlife viewing and relaxing drives. We’re excited to experience its iconic glaciers, rugged peaks, and crystal-clear lakes during the summer months, which are the ideal time to visit.</p>
+                <div className={styles.destinationRow}>
+                    <p>This years upcoming vacation destination is Montana, where Sarah and I will be staying in Whitefish and exploring the Glacier National Park. Spanning over more than 1 million acres, Glacier is the crown jewel of a vast protected wilderness shared by the U.S. and Canada.</p>
+                </div>
+                <div>
+                    <p>With more than 700 miles of hiking trails, the park offers endless opportunities for adventure, from scenic hikes and boating on alpine lakes to wildlife viewing and relaxing drives. We’re excited to experience its iconic glaciers, rugged peaks, and crystal-clear lakes during the summer months, which are the ideal time to visit.</p>
+                </div>
             </div>
             <div className={styles.weatherContainer}>
                 <WeatherCard />

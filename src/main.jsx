f67a47c9c87fa7ae67d3602936c1a8086b29ad7c
Tim Baines <timbaines@timbaines.com>
2025-07-17 21:28:28 -0400

    feat: wrap `App` in `ThemeProvider` for global theme context support

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from "@/context/ThemeContext";
 import App from '@/App.jsx'
 
+/***** GLOBAL STYLES *****/
 import '@/styles/globals.css'
 import '@/styles/typography.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider>
+        <App />
+    </ThemeProvider>
+  </StrictMode>
 )

b1e6dc6c813dab303b06e7caeb9b6d5d72fd2882
Tim Baines <timbaines@timbaines.com>
2025-07-20 15:20:08 -0400

    chore: replace `children` prop with `Outlet` in `MainLayout` for nested routing

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
-export default function MainLayout({ children }) {
+export default function MainLayout() {
     return (
         <>
             <Header />
-               <main>
-                {children}
-               </main>
+            <main>
+                <Outlet />
+            </main>
             <Footer />
         </>
     );

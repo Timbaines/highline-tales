8879dec582ffb16a3a9f1d43fe9275790aeb4ca4
Tim Baines <timbaines@timbaines.com>
2025-07-20 17:39:53 -0400

    chore: remove unused `ActivityList.module.css` and update `ActivityList` to inline styles

--- src/components/blog/ActivityList.jsx
+++ src/components/blog/ActivityList.jsx
@@ -1,18 +1,13 @@
-import ActivityCard from '@/components/blog/ActivityCard.jsx';
-import { activitiesData } from '@/data/activities-data.js';
-
-/***** MODULE STYLES *****/
-import styles from '@/components/blog/ActivityList.module.css'
+import ActivityCard from '@/components/blog/ActivityCard';
+import { activitiesData } from '@/data/activitiesData.js';
 
 export default function ActivityList() {
     return (
-        <div className={styles.activityListGridContainer}>
-            <div>
-                <h3>Activities</h3>
-                {activitiesData.map(activity => (
-                    <ActivityCard key={activity.id} activity={activity} />
-                ))}
-            </div>
+        <div>
+            <h3>Activities</h3>
+            {activitiesData.map(activity => (
+                <ActivityCard key={activity.id} activity={activity} />
+            ))}
         </div>
     )
 }

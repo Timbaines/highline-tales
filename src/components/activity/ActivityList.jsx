775dbccbb4015d05a6807095e5b4fed9c30bdc1d
Tim Baines <timbaines@timbaines.com>
2025-07-28 17:21:23 -0400

    refactor: rename `blog` to `activity` in `ActivityList` module and update imports
    - Harmonize directory structure and component naming for better clarity.
    - Replace `div` wrapper with fragment for cleaner output.

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -1,13 +1,12 @@
-import ActivityCard from '@/components/blog/ActivityCard';
+import ActivityCard from '@/components/activity/ActivityCard';
 import { activitiesData } from '@/data/activitiesData.js';
 
 export default function ActivityList() {
     return (
-        <div>
-            <h3>Activities</h3>
+        <>
             {activitiesData.map(activity => (
                 <ActivityCard key={activity.id} activity={activity} />
             ))}
-        </div>
+        </>
     )
 }

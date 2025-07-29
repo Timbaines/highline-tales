8e982a4fafc31c0b4d4a0bbdd462767fd8db6276
Tim Baines <timbaines@timbaines.com>
2025-07-29 11:36:44 -0400

    feat: add `ActivitiesPage` component with responsive grid layout
    - Introduce `ActivitiesPage.jsx` component with `ActivityList` integration.
    - Add modular CSS for responsive grid layout and mobile optimization.

---
+++ src/pages/ActivitiesPage.jsx
@@ -0,0 +1,16 @@
+import ActivityList from '@/components/activity/ActivityList';
+
+/***** MODULE STYLES *****/
+import styles from '@/pages/ActivitiesPage.module.css';
+
+export default function ActivitiesPage() {
+  return (
+      <>
+          <h2>Activities</h2>
+          <div className={styles.activitiesContainer}>
+              <ActivityList />
+          </div>
+      </>
+
+  )
+}

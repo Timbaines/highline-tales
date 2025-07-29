388a0d64cab446bee779151b35c539d58dc0b7c0
Tim Baines <timbaines@timbaines.com>
2025-07-29 14:10:47 -0400

    feat: enhance `activitiesData` structure with additional fields and standardized tags
    - Add `title`, `heading`, and `content` fields for enriched activity details.
    - Replace `features` with `tag` arrays for improved categorization.
    - Standardize formatting and descriptions for consistency.

--- src/data/activitiesData.js
+++ src/data/activitiesData.js
@@ -1,45 +1,69 @@
 export const activitiesData = [
     {
         "id": 1,
-        "image": "/base-camp-kayaking.webp",
+        "title": "Lake Kayaking",
         "subtitle": "Base Camp Bigfork, MT",
-        "title": "River Kayaking",
+        "heading": "Flathead Lake",
+        "image": "/base-camp-kayaking.webp",
         "miles": 4,
         "difficulty": "Easy",
         "date": "08/14/25",
         "description": "Kayaking on Flathead Lake and paddling through one of Montana’s most stunning natural landscapes.",
         "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequatur delectus dolore earum est hic, quisquam voluptatem? Ab at atque autem deleniti eaque error fuga harum laudantium nisi perspiciatis quis repellat suscipit, velit. A aliquam at blanditiis dignissimos dolorem dolorum ea, eaque et fugit illum inventore itaque libero minus nostrum officia officiis, pariatur porro quibusdam quisquam recusandae similique tempore ut vel? A doloribus, eligendi est incidunt necessitatibus non reprehenderit temporibus voluptate. Cum facere perferendis recusandae suscipit temporibus! Accusamus doloribus earum eligendi error esse fugiat harum illum laboriosam molestiae mollitia, nihil officia omnis perspiciatis, possimus praesentium quae qui similique sint voluptatum!",
-        "features": ["Kayaking", "Paddling", "Water Sports"]
-
+        "tag": [
+            "Kayaking",
+            "Paddling",
+            "Water Sports"
+        ]
     },
     {
         "id": 2,
-        "image": "/highline-trail-hero.webp",
-        "subtitle": "Glacier National Park",
         "title": "Highline Trail",
+        "subtitle": "Glacier National Park",
+        "heading": "Guided Hiking Tour",
+        "image": "/highline-trail-hero.webp",
         "miles": 6,
         "difficulty": "Moderate",
         "date": "08/16/25",
-        "description": "Starting our day with an 8 hour guided hiking tour. We will learn about the geology, wildlife and history on the way up."
+        "description": "Starting our day with an 8 hour guided hiking tour. We will learn about the geology, wildlife and history on the way up.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequatur delectus dolore earum est hic, quisquam voluptatem? Ab at atque autem deleniti eaque error fuga harum laudantium nisi perspiciatis quis repellat suscipit, velit. A aliquam at blanditiis dignissimos dolorem dolorum ea, eaque et fugit illum inventore itaque libero minus nostrum officia officiis, pariatur porro quibusdam quisquam recusandae similique tempore ut vel? A doloribus, eligendi est incidunt necessitatibus non reprehenderit temporibus voluptate. Cum facere perferendis recusandae suscipit temporibus! Accusamus doloribus earum eligendi error esse fugiat harum illum laboriosam molestiae mollitia, nihil officia omnis perspiciatis, possimus praesentium quae qui similique sint voluptatum!",
+        "tag": [
+            "Kayaking",
+            "Paddling",
+            "Water Sports"
+        ]
     },
     {
         "id": 3,
-        "image": "/st-mary-lake-tour.webp",
-        "subtitle": "St. Mary Lake",
         "title": "Rising Sun Boat Tour",
+        "subtitle": "St. Mary Lake",
+        "heading": "Rising Sun Boat Tour",
+        "image": "/st-mary-lake-tour.webp",
         "miles": 5,
         "difficulty": "Easy",
         "date": "08/17/25",
-        "description": "Time to Enjoy stunning peaks and glacial waters on a St. Mary Lake tour with Glacier Park Boat Company."
+        "description": "Time to enjoy stunning peaks and glacial waters on a St. Mary Lake tour with Glacier Park Boat Company.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequatur delectus dolore earum est hic, quisquam voluptatem? Ab at atque autem deleniti eaque error fuga harum laudantium nisi perspiciatis quis repellat suscipit, velit. A aliquam at blanditiis dignissimos dolorem dolorum ea, eaque et fugit illum inventore itaque libero minus nostrum officia officiis, pariatur porro quibusdam quisquam recusandae similique tempore ut vel? A doloribus, eligendi est incidunt necessitatibus non reprehenderit temporibus voluptate. Cum facere perferendis recusandae suscipit temporibus! Accusamus doloribus earum eligendi error esse fugiat harum illum laboriosam molestiae mollitia, nihil officia omnis perspiciatis, possimus praesentium quae qui similique sint voluptatum!",
+        "tag": [
+            "Kayaking",
+            "Paddling",
+            "Water Sports"
+        ]
     },
     {
         "id": 4,
+        "title": "Western Alpine Tour",
+        "subtitle": "Red Bus",
+        "heading": "Coasting the Western Alpine",
         "image": "/red-bus-tour.webp",
-        "subtitle": "Western Side",
-        "title": "Alpine Red Bus Tour",
         "miles": 50,
         "difficulty": "Easy",
         "date": "08/18/25",
-        "description": "Excited to experience stunning views on a Red Bus Tour through the GNP West Side Alpine."
+        "description": "Excited to experience stunning views on a Red Bus Tour through the GNP West Side Alpine.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequatur delectus dolore earum est hic, quisquam voluptatem? Ab at atque autem deleniti eaque error fuga harum laudantium nisi perspiciatis quis repellat suscipit, velit. A aliquam at blanditiis dignissimos dolorem dolorum ea, eaque et fugit illum inventore itaque libero minus nostrum officia officiis, pariatur porro quibusdam quisquam recusandae similique tempore ut vel? A doloribus, eligendi est incidunt necessitatibus non reprehenderit temporibus voluptate. Cum facere perferendis recusandae suscipit temporibus! Accusamus doloribus earum eligendi error esse fugiat harum illum laboriosam molestiae mollitia, nihil officia omnis perspiciatis, possimus praesentium quae qui similique sint voluptatum!",
+        "tag": [
+            "Tour",
+            "Travel"
+        ]
     }
 ]

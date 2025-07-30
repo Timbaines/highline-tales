9b643962f9a9e66ca3c74a80b15496e00ba9c19d
Tim Baines <timbaines@timbaines.com>
2025-07-30 15:57:37 -0400

    refactor: update `HomePageLayout` props and grid structure for simplified layout
    - Replace `leftTopContent`, `leftBottomContent`, `rightTopContent`, and `rightBottomContent` props with `leftGrid` and `rightGrid`.
    - Adjust grid styles in `HomePageLayout.module.css` for improved responsiveness.
    - Remove `Destination` component and incorporate its content directly into `HomePage` with updated structure.
    - Simplify `HomePage` layout and import structure for better maintainability.

--- src/layouts/HomePageLayout.jsx
+++ src/layouts/HomePageLayout.jsx
@@ -2,27 +2,21 @@
 import styles from '@/layouts/HomePageLayout.module.css';
 
 export default function HomePageLayout({
-      leftTopContent,
-      leftBottomContent = null,
-      rightTopContent = null,
-      rightBottomContent = null,
-      fullWidth = false
+    leftGrid= null,
+    rightGrid = null,
   }) {
 
     return (
         <section>
-            <div className={`${styles.gridContainer} ${fullWidth ? styles.fullWidth : ''}`}>
-                <div className={styles.leftTop}>{leftTopContent}</div>
-                {leftBottomContent && (
-                    <div className={styles.leftBottom}>{leftBottomContent}</div>
+            <div className={styles.gridContainer}>
+                {leftGrid && (
+                    <div className={styles.leftGrid}>{leftGrid}</div>
                 )}
-                {rightTopContent && (
-                    <div className={styles.rightTop}>{rightTopContent}</div>
-                )}
-                {rightBottomContent && (
-                    <div className={styles.rightBottom}>{rightBottomContent}</div>
+
+                {rightGrid && (
+                    <div className={styles.rightGrid}>{rightGrid}</div>
                 )}
             </div>
         </section>
     );
-}
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,34 @@
-import Destination from '@/components/Destination';
 import WeatherCard from '@/components/ui/WeatherCard.jsx';
-import MapContainer from '@/components/map/MapContainer.jsx';
-import ActivityList from '@/components/blog/ActivityList';
-import SectionLayout from '@/layouts/SectionLayout';
+import ActivityList from '@/components/activity/ActivityList';
+import HomePageLayout from '@/layouts/HomePageLayout.jsx';
+import DemoComponent from "@/components/DemoComponent.jsx";
+
+/***** MODULE STYLES *****/
+import styles from '@/pages/HomePage.module.css';
+
 
 export default function HomePage() {
     return (
-        <SectionLayout
-            leftTopContent={<Destination />}
-            leftBottomContent={<ActivityList />}
-            rightTopContent={<WeatherCard />}
-            rightBottomContent={<MapContainer />}
-        />
-    );
-}
+        <>
+            <section className={styles.homePageContainer}>
+                <div className={styles.homePageContent}>
+                    <h2>Destination</h2>
+                    <p>This years upcoming vacation destination is Montana, where Sarah and I will be staying in Whitefish and exploring the Glacier National Park. Spanning over more than 1 million acres, Glacier is the crown jewel of a vast protected wilderness shared by the U.S. and Canada.With more than 700 miles of hiking trails, the park offers endless opportunities for adventure, from scenic hikes and boating on alpine lakes to wildlife viewing and relaxing drives.</p>
+                    <p>This years upcoming vacation destination is Montana, where Sarah and I will be staying in Whitefish and exploring the Glacier National Park. Spanning over more than 1 million acres, Glacier is the crown jewel of a vast protected wilderness shared by the U.S. and Canada.</p>
+                </div>
+                <div className={styles.homePageForecast}>
+                    <WeatherCard />
+                </div>
+            </section>
 
+            <section>
+                <h2>Activities</h2>
+                <HomePageLayout
+                    leftGrid={<ActivityList />}
+                    rightGrid={<DemoComponent />}
+                />
+            </section>
+
+        </>
+    );
+};

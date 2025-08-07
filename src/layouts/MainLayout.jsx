ab5330fc715fd2771cb1f1fbb1ac96e11e1010cb
Tim Baines <timbaines@timbaines.com>
2025-08-07 01:05:11 -0400

    style: update `MainLayout` structure and styles
    - Add `SubpageHeader` for non-homepage routes and conditionally render it.
    - Wrap layout in `mainLayoutContainer` class for consistent styling.
    - Use `Container` component to standardize layout structure.
    - Adjust imports for new CSS module and layout updates.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/Header';
+import SubpageHeader from '@/components/SubpageHeader';
 import Footer from '@/components/Footer';
 import VisitorCounter from "@/components/ui/VisitorCounter.jsx";
+import { Container } from '@/layouts/components';
+
+import styles from '@/layouts/MainLayout.module.css';
 
 export default function MainLayout() {
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
     return (
-        <>
-            <Header />
+        <div className={styles.mainLayoutContainer}>
+            {isHomePage ? <Header /> : <SubpageHeader />}
             <main>
-                <Outlet />
+                <Container variant="default">
+                    <Outlet />
+                </Container>
             </main>
             <VisitorCounter />
             <Footer />
-        </>
+        </div>
     );
-}
+};

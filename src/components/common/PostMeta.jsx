a47a1870df8e49c15dd7225f377b09f8a908c565
Tim Baines <timbaines@timbaines.com>
2025-08-10 17:21:42 -0400

    style: adjust `PostMeta` styles for spacing and font size consistency

--- src/components/common/PostMeta.jsx
+++ src/components/common/PostMeta.jsx
@@ -2,7 +2,7 @@ import { FaCalendarAlt } from 'react-icons/fa';
 
 export default function PostMeta({ date, author, className = '' }) {
   return (
-    <div className={className} style={{ marginBottom: '2rem' }}>
+    <div className={className} style={{ marginTop: '.75rem', marginBottom: '2rem', fontSize: '0.875rem' }}>
       {author && <span>By: {author}</span>}
       {date && <span> | <FaCalendarAlt size={12} /> {date}</span>}
     </div>

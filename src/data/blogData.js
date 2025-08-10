79b5ddc59ef02e8661e5bf703fd89692c8e32b22
Tim Baines <timbaines@timbaines.com>
2025-08-10 17:00:46 -0400

    feat: add `blogData` with sample blog entries for Montana trip
    - Introduced `blogData` file containing structured data for eight sample blog posts.
    - Included metadata such as title, subtitle, author, date, image, tags, slug, excerpt, and content.

---
+++ src/data/blogData.js
@@ -0,0 +1,202 @@
+export const blogData = [
+    {
+        id: 1,
+        title: "Flight to Montana",
+        subtitle: "Day 1",
+        author: "Sarah & Tim Baines",
+        date: "08/13/25",
+        slug: "montana-vacation-day-1-travel-to-montana",
+        excerpt: "Day 1: Traveling",
+        content: "[h2]Day 1[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Travel", "Airbnb"]
+    },
+    {
+        id: 2,
+        title: "Flathead Lake",
+        subtitle: "TRIP PLANNING",
+        author: "Sarah & Tim Baines",
+        date: "08/14/25",
+        slug: "montana-day-2-exploring-bigfork",
+        excerpt: "Day 2: Kayaking",
+        content: "[h2]Day 2[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Trips", "Glacier"]
+    },
+    {
+        id: 3,
+        title: "Wildlife Watching",
+        subtitle: "TRIP PLANNING",
+        author: "Sarah & Tim Baines",
+        date: "08/15/25",
+        slug: "montana-vacation-day-3-wildlife-watching",
+        excerpt: "Day 3: Exploring",
+        content: "[h2]Day 3[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Trips", "Glacier"]
+    },
+    {
+        id: 4,
+        title: "Highline Trail",
+        subtitle: "TRIP PLANNING",
+        author: "Sarah & Tim Baines",
+        date: "08/16/25",
+        slug: "montana-vacation-day-4-highline-trail",
+        excerpt: "Day 4: Hiking",
+        content: "[h2]Day 4[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Trips", "Glacier"]
+    },
+    {
+        id: 5,
+        title: "St. Mary Lake",
+        subtitle: "TRIP PLANNING",
+        author: "Sarah & Tim Baines",
+        date: "08/17/25",
+        slug: "montana-vacation-day-5-glacier-national-park",
+        excerpt: "Day 5: Boat Tour",
+        content: "[h2]Day 5[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Trips", "Glacier"]
+    },
+    {
+        id: 6,
+        title: "Western Alpine",
+        subtitle: "TRIP PLANNING",
+        author: "Sarah & Tim Baines",
+        date: "08/18/25",
+        slug: "montana-vacation-day-6-western-alpine-tour",
+        excerpt: "Day 6: West Glacier",
+        content: "[h2]Day 6[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Trips", "Glacier"]
+    },
+    {
+        id: 7,
+        title: "Big Sky Tour",
+        subtitle: "TRIP PLANNING",
+        author: "Sarah & Tim Baines",
+        date: "08/19/25",
+        slug: "montana-vacation-day-7-lakeside-relaxation",
+        excerpt: "Day 7: East Glacier",
+        content: "[h2]Day 7[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Trips", "Glacier"]
+    },
+    {
+        id: 8,
+        title: "Heading Home",
+        subtitle: "TRIP PLANNING",
+        author: "Sarah & Tim Baines",
+        date: "08/20/25",
+        slug: "montana-vacation-day-8-travel-home",
+        excerpt: "Day 8: Traveling",
+        content: "[h2]Day 8[/h2]<br><br>" +
+            "Excerpt<br><br>" +
+            "Paragraph...<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Highline Trail\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Mountain goat\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>" +
+            "[img src=\"/mtn-vector-2.png\" alt=\"Alpine wildflowers\" caption=\"Caption\" size=\"full\"]<br><br>" +
+            "[h3]H3[/h3]<br><br>" +
+            "Paragraph...<br><br>",
+        image: "/mtn-vector-2.png",
+        tags: ["Planning", "Trips", "Glacier"]
+    },
+];

4cb25059180b8c26ca034b093075fc65866afcda
Tim Baines <timbaines@timbaines.com>
2025-08-11 00:10:06 -0400

    chore: update asset paths in Footer and set up CI workflow
    - Update image paths in `Footer.jsx` for consistency using `/src/assets`.
    - Add GitHub Actions CI workflow for linting, testing, and dependency installation.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,19 +13,19 @@ export default function Footer() {
                 </Link>
                 <div className={styles.footerTechStack}>
                     <a href="https://vite.dev" target="_blank">
-                        <img src="/vite.svg"
+                        <img src="/src/assets/vite.svg"
                              className={styles.footerTechIcons}
                              alt="Vite logo"
                         />
                     </a>
                     <a href="https://react.dev/" target="_blank">
-                        <img src="/react_dark.svg"
+                        <img src="/src/assets/react_dark.svg"
                             className={styles.footerTechIcons}
                              alt="React logo"
                         />
                     </a>
                     <a href="https://supabase.com/" target="_blank">
-                        <img src="/supabase.svg"
+                        <img src="/src/assets/supabase.svg"
                             className={styles.footerTechIcons}
                              alt="Supabase logo"
                         />
